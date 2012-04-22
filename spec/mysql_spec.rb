b2afd06095b15011a54cc772c7aacba7a4d829b8
TOMITA Masahiro <tommy@tmtm.org>
2012-04-23 01:33:43 +0900

    version 2.9.7

--- spec/mysql_spec.rb
+++ spec/mysql_spec.rb
@@ -13,7 +13,7 @@ MYSQL_SOCKET   = ENV['MYSQL_SOCKET']
 
 describe 'Mysql::VERSION' do
   it 'returns client version' do
-    Mysql::VERSION.should == 20906
+    Mysql::VERSION.should == 20907
   end
 end
 
