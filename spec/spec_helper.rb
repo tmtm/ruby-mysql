33c8585fd0cc1a140229a90f9ad8f6aaa52d4ef8
TOMITA Masahiro <tommy@tmtm.org>
2022-11-03 19:08:57 +0900

    put connection parameter to config.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,16 +3,11 @@ require 'rspec'
 # for power_assert
 Fixnum = Integer unless defined? Fixnum
 Bignum = Integer unless defined? Bignum
-
 require 'rspec-power_assert'
+RSpec::PowerAssert.example_assertion_alias :assert
+
 require 'mysql'
 
-# MYSQL_USER must have ALL privilege for MYSQL_DATABASE.* and RELOAD privilege for *.*
-MYSQL_SERVER   = ENV['MYSQL_SERVER']
-MYSQL_USER     = ENV['MYSQL_USER']
-MYSQL_PASSWORD = ENV['MYSQL_PASSWORD']
-MYSQL_DATABASE = ENV['MYSQL_DATABASE'] || "test_for_mysql_ruby"
-MYSQL_PORT     = ENV['MYSQL_PORT']
-MYSQL_SOCKET   = ENV['MYSQL_SOCKET']
-
-RSpec::PowerAssert.example_assertion_alias :assert
+conf = "#{__dir__}/config.rb"
+File.write(conf, File.read("#{conf}.example")) unless File.exist? conf
+load conf
